ae58284f47ca577826680cf9fd431f05a8aaa634
marahirodriguez <marah@hotmail.es>
2015-07-03 12:44:28 -0500

    a
    a

--- SIHECE/Formularios/frmSplash.Designer.cs
+++ SIHECE/Formularios/frmSplash.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 307);
+            this.Name = "frmSplash";
             this.Text = "frmSplash";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
